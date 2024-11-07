ca85e74540f5ed97b73e6223c147367ba4db3c6b
aannabottini <anna.bottini@alessandrinimainardi.edu.it>
2024-11-07 16:20:56 +0100

    Add files via upload
    domanda 9 processi sequenziali e paralleli

---
+++ dormiMacina.py
@@ -0,0 +1,56 @@
+import os
+import time
+import threading
+import multiprocessing
+
+NUM_WORKERS = 4
+
+def only_sleep ():
+    '''Non fa nulla, aspetta lo scadere del timer per terminare'''
+    print("PID: %s, Process Name: %s, Thread Name: %s " %(
+        os.getpid(),
+        multiprocessing.current_process().name, 
+        threading.curret_thread().name)
+    )
+    time.sleep(1)
+
+def crunch_numbers():
+    '''Esegue dei calcoli'''
+    print("PID: %s, Process Name: %s, Thread Name: %s " %(
+        os.getpid(),
+        multiprocessing.current_process().name, 
+        threading.curret_thread().name)
+    )
+    x=0
+    while x < 10000000: 
+        x += 1
+
+if __name__ == "__main__":
+    #Esegue i task in serie
+    start_time = time.time()
+    for _ in range (NUM_WORKERS):
+        only_sleep()
+        #crunch_numbers()
+    end_time = time.time()
+
+    print("Serial time = ", end_time - start_time)
+
+    #Esegue i task usando i threads
+    start_time = time.time()
+    threads = [threading.Thread(target=only_sleep) for _ in range(NUM_WORKERS)]
+    #threads = [threading.Thread(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
+    end_time = time.time()
+
+    print("Threads time = ", end_time - start_time)
+
+    #Esegue i task usando i processes
+    start_time = time.time()
+    processes = [multiprocessing.Process(target=only_sleep) for _ in range(NUM_WORKERS)]
+    #processes = [multiprocessing.Process(target=crunch_numbers) for _ in range(NUM_WORKERS)]
+    [process.start() for process in processes]
+    [process.join() for process in processes]
+    end_time = time.time()
+
+    print("Parallel time = ", end_time - start_time)
